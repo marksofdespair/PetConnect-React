ef2b6caac04366169e7a38e460fd41fd7a1b7bed
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-03-23 15:21:29 -0500

    nav bar basics

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,12 @@
-
+import { Link } from "react-router-dom"
 
 export function NavBar() {
-    
+    return (
+        <>
+            <Link to="/">Home</Link>
+            <Link to="/AboutUs">About Us</Link>
+            <Link to="/Settings"></Link>
+            <Link to="/ServiceSearch"></Link>
+        </>
+    )
 }
