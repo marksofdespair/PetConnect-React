a7f5f2152b64cac9328aae875cf0bfa06d0e6830
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-03-28 19:34:35 -0500

    added boilerplate for AddPet and routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Login from './Pages/Login';
 import UserHome from './Pages/User/UserHome';
 import ServiceSearch from './Pages/ServiceSearch'; 
 import AddService from './Pages/Provider/AddService';
+import AddPet from './Pages/User/AddPet';
 import Settings from './Pages/Provider/Settings';
 import AboutUs from './Pages/AboutUs';
 import CreateAccount from './Pages/CreateAccount';
 import ProviderProfile from './Pages/Provider/ProviderProfile';
 import UserProfile from './Pages/User/UserProfile';
+import UserSettings from './Pages/User/UserSettings';
 import './App.css';
 import './Layout';
 
@@ -25,7 +27,9 @@ const App = () => {
             <Route path="/UserHome" element={<UserHome />} /> {/* Route for UserHome */}
             <Route path="/ServiceSearch" element={<ServiceSearch />} /> {/* Route for ServiceSearch */}
             <Route path="/AddService" element={<AddService />} /> {/* Route for AddService */}
-            <Route path="/Settings" element={<Settings />} /> {/* Route for Settings */}
+            <Route path="/AddPet" element={<AddPet />} /> {/* Route for AddPet */}
+            <Route path="/Settings" element={<Settings />} /> {/* Route for Provider Settings */}
+            <Route path="/UserSettings" element={<UserSettings />} /> {/* Route for User Settings */}
             <Route path="/AboutUs" element={<AboutUs />} /> {/* Route for AddService */}
             <Route path="/CreateAccount" element={<CreateAccount />} /> {/* Route for CreateAccount */}
             <Route path="/ProviderProfile" element={<ProviderProfile />} /> {/* Rout for ProviderProfile */}
