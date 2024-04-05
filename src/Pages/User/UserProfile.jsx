9444556311026f2df32e8ed51d439791c7d5a9ba
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-04-04 19:49:39 -0500

    Amended UserProfile

--- src/Pages/User/UserProfile.jsx
+++ src/Pages/User/UserProfile.jsx
@@ -12,22 +12,7 @@ const UserProfile = ({ User }) => {
 
       {/* Should display User name */}
       <h2>{name}</h2>
-
-      {/* Modify for Users titles/training */}
       <div>
-        <h3>Titles/Training:</h3>
-        <p>{titles}</p>
-      </div>
-
-      {/* Modify for Reviews feature */}
-      <div>
-        <h3>Reviews:</h3>
-        {reviews.map((review, index) => (
-          <div key={index}>
-            <p>{review.comment}</p>
-            <p>Rating: {review.rating}</p>
-          </div>
-        ))}
       </div>
     </div>
   );
@@ -37,13 +22,6 @@ UserProfile.propTypes = {
   User: PropTypes.shape({
     name: PropTypes.string.isRequired,
     icon: PropTypes.string, // URL for the User's icon
-    titles: PropTypes.string, // Training/titles of the User
-    reviews: PropTypes.arrayOf(
-      PropTypes.shape({
-        comment: PropTypes.string.isRequired,
-        rating: PropTypes.number.isRequired,
-      })
-    ),
   }).isRequired,
 };
 
