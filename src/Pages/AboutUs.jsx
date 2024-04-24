46e7596691f4bc83171ff56eb29b2a9fec61c444
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-04-23 21:02:19 -0500

    Just for fun - added more to AboutUs, sign your name :]

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,7 +4,25 @@ import { Link } from 'react-router-dom';
 const AboutUs = () => {
     return (
         <div>
-            PetConnect is a Capstone Project created by the Team Laika capstone project group for the 2024 Full-Stack web developer program.
+            PetConnect is a Capstone Project created by the Team Laika capstone project group for the May 2024 Full-Stack web developer program.
+            <br></br>
+            <br></br>
+ 
+                <p><h1>The Techstack</h1></p>
+                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>Java</li>
+                    <li>Spring Boot</li>
+                </ul>
+
+                <p><h1>The Team</h1></p>
+                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                    <li>[Sign Your Name Here!]</li>
+                    <li>[Sign Your Name Here!]</li>
+                    <li>[Sign Your Name Here!]</li>
+                    <li><a href="https://github.com/marksofdespair" target="_blank" rel="noopener noreferrer">Dia - Marksofdespair</a></li>
+                </ul>
         </div>
     )
 }
