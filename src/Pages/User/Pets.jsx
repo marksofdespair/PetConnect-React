7428a279bd224deec5e3d512344ea9c22a87d757
terryweis <weist2088@gmail.com>
2024-04-25 14:03:56 -0500

    Pet Changed - Not working

--- src/Pages/User/Pets.jsx
+++ src/Pages/User/Pets.jsx
@@ -1,33 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios'; 
+import axios from 'axios';
 
+let username = localStorage.getItem('username');
+// Check if username exists
+if (!username) {
+  throw new Error('Username not found in local storage');
+}
+//const authStr = 'Bearer + 4521J7';
+username = username.replace(/^"|"$/g, '');
 function Pets() {
   const [pets, setPets] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  
   useEffect(() => {
-    // Fetch pet data from the backend - only update if port or api URL changes.
-    axios.get('http://localhost:8080/api/pets') 
-      .then(response => {
-        setPets(response.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
+    const fetchpets = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/pets');
+        console.log('Response from API:', response.data);
+        setBreeds(response.data);
+      } catch (error) {
+        console.error('Error fetching dog breeds:', error);
+      }
+    };
 
+    fetchpets();
+  }, []);
+  
+    // Fetch pet data from the backend - only update if port or api URL changes.
   if (loading) {
     return <div>Loading...</div>;
   }
-
+  
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-
+  
   return (
     <div>
       <h1>My Pets</h1>
@@ -47,5 +56,4 @@ function Pets() {
     </div>
   );
 }
-
 export default Pets;
