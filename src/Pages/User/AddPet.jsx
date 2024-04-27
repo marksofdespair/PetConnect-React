4be787fabb736fee70b7a4d767243d272134bc58
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-04-27 13:19:02 -0500

    Changed decode imports

--- src/Pages/User/AddPet.jsx
+++ src/Pages/User/AddPet.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'; 
-import jwtDecode from 'jwt-decode';
+import * as jwtDecode from 'jwt-decode';
 
 function Pets() {
   const [pets, setPets] = useState([]);
